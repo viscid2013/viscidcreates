f2e0216e7cfe9916246385fc4812d08f8c6cf5c8
Ben McFadden <viscid07@gmail.com>
2019-06-04 15:32:04 -0400

    Added size select and add to cart to slides - realted styling and layout changes - work in progress

--- v_portfolio/components/query_slides.php
+++ v_portfolio/components/query_slides.php
@@ -22,14 +22,28 @@ try {
 			$favs[$ii] = $array[$ii]["num_favs"];
 			$iid[$ii] = $array[$ii]["iid"];
 			
-			echo '<div class="w3-display-container vcSlides" style="font-size:130%;">
-				<input id="slide_' . $iid[$ii] . '" value="' . ($ii +1) . '" style="display: none;">
-			  <img id="slideImg_' . ($ii +1) . '" class="vcSlidesImg" src="../images/' . $img[$ii] . '" style="width:100%">
-			  <div class="w3-display-topleft w3-theme-l3 w3-opacity-min w3-padding w3-center w3-cell-row slidesBar" style="width: 100%;">
+			echo '<div class="vcSlides" style="font-size:130%;">
+			<div class="w3-theme-l3 w3-padding w3-center w3-cell-row slidesBar" style="width: 100%; z-index:2;">
 				<div class="w3-cell"><span class="vcicon icon-favoritesvc"></span>&nbsp;' . $favs[$ii] . '</div>
 				<div class="w3-cell"><span class="vcicon icon-commentsvc"></span>&nbsp;' . $favs[$ii] . '</div>
 				<div class="w3-cell"><span class="vcicon icon-sharevc"></span></div>
 			  </div>
+				<input id="slide_' . $iid[$ii] . '" value="' . ($ii +1) . '" style="display: none;">
+			  <img id="slideImg_' . ($ii +1) . '" class="vcSlidesImg w3-border" src="../images/' . $img[$ii] . '" style="width:100%">
+	
+			<div class="w3-cell-row orderBar" style="width: 100%; z-index:2; font-size:90%;">  
+			<div class="w3-cell w3-padding" style="width: 75%">
+			  <select class="w3-select" name="option">
+				  <option value="" disabled selected>Choose your option</option>
+				  <option value="1">4X6</option>
+				  <option value="2">5X7</option>
+				  <option value="3">8X10</option>
+				</select>
+			</div>
+			<div class="w3-cell w3-padding" style="width: 25%">
+				<div class="w3-button w3-block w3-theme-action">Add to Cart</div>
+			</div>
+			</div>
 			</div>';
 			
 
