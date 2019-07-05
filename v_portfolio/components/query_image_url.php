d7520c43954555f2fa84bc5ca1b7535b573e3f6f
Ben McFadden <viscid07@gmail.com>
2019-07-05 19:07:57 -0400

    another tweak to another thing

--- v_portfolio/components/query_image_url.php
+++ v_portfolio/components/query_image_url.php
@@ -30,7 +30,7 @@ try {
 
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
 	
-	$result = "http://beamcreates.com/v_portfolio/query_image_url.php?imgDiv=yes";
+	$result = "http://beamcreates.com/v_portfolio/pages/vc_home.php";
 	
 	echo $result;
 	
