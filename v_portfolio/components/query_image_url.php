6cf3882b0088bc888e4dee384c85b2b8affbe31c
Ben McFadden <viscid07@gmail.com>
2019-07-05 16:48:35 -0400

    and again - image page edit for live fb share testing

--- v_portfolio/components/query_image_url.php
+++ v_portfolio/components/query_image_url.php
@@ -1,5 +1,13 @@
 <?php
 
+// Program to display current page URL. 
+  
+$link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 
+                "https" : "http") . "://" . $_SERVER['HTTP_HOST'] .  
+                $_SERVER['REQUEST_URI']; 
+  
+echo $link; 
+
 if (isset($_REQUEST['iid'])){
 	$iid = $_REQUEST['iid'];
 }
@@ -22,7 +30,7 @@ try {
 
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
 	
-	$result = "http://beamcreates.com/v_portfolio/images/" . $result["image_link"] . "?imgDiv=yes";
+	$result = $link . "?imgDiv=yes";
 	
 	echo $result;
 	
