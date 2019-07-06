c68e6a7298bc8448d7dec5e1571c930266ffde0c
Ben McFadden <viscid07@gmail.com>
2019-07-06 06:33:35 -0400

    updated type to content website

--- v_portfolio/components/show_image.php
+++ v_portfolio/components/show_image.php
@@ -28,7 +28,7 @@ try {
 <meta charset="utf-8">
 	
 <meta property="og:url"                content="http://beamcreates.com/v_portfolio/pages/vc_home.php" />
-<meta property="og:type"               content="images" />
+<meta property="og:type"               content="website" />
 <meta property="og:title"              content="<?php echo $result['title']; ?>" />
 <meta property="og:description"        content="Arts and such" />
 <meta property="og:image"              content="http://beamcreates.com/v_portfolio/images/<?php echo $result['image_link']; ?>" />
