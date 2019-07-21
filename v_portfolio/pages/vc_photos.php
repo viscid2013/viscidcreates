91a20b7840171f5a28360dd911ea39f905672ce6
Ben McFadden <viscid07@gmail.com>
2019-07-21 12:29:39 -0400

    more room again for x button in share box

--- v_portfolio/pages/vc_photos.php
+++ v_portfolio/pages/vc_photos.php
@@ -40,8 +40,8 @@ function doTumb(tVal){
 
 
 <div id="share" class="w3-border w3-white w3-card w3-padding-small w3-center w3-container viewShare">
-	<span class="w3-medium w3-button w3-display-topright" onClick="document.getElementById('share').style.display = 'none';" style="font-size: 110%;">&times;</span>
-	<div class="w3-container w3-theme w3-padding-small" style="margin-top: 30px">
+	<span class="w3-medium w3-button w3-display-topright" onClick="document.getElementById('share').style.display = 'none';" style="font-size: 120%;">&times;</span>
+	<div class="w3-container w3-theme w3-padding-small" style="margin-top: 40px">
 		Share it!
 	</div>
 		
