aab7e23c1a44d8d152fb703f6d96c246362c4a09
Ben McFadden <viscid07@gmail.com>
2019-07-21 12:47:53 -0400

    more tweaks to share box header - larger bold font for title

--- v_portfolio/pages/vc_photos.php
+++ v_portfolio/pages/vc_photos.php
@@ -41,7 +41,7 @@ function doTumb(tVal){
 
 <div id="share" class="w3-border w3-white w3-card w3-padding-small w3-center w3-container viewShare">
 	<span class="w3-medium w3-button w3-display-topright" onClick="document.getElementById('share').style.display = 'none';" style="font-size: 120%;">&times;</span>
-	<div class="w3-container w3-text-theme w3-padding-small w3-display-topleft">
+	<div class="w3-container w3-text-theme w3-padding w3-display-topleft vcBold" style="font-size: 120%;">
 		Share it!
 	</div>
 		
