613197c63962ec07f8e0d1b4ba3fb5c21167673a
Ben McFadden <viscid07@gmail.com>
2019-07-21 12:54:33 -0400

    margin top to share row

--- v_portfolio/pages/vc_photos.php
+++ v_portfolio/pages/vc_photos.php
@@ -41,10 +41,11 @@ function doTumb(tVal){
 
 <div id="share" class="w3-border w3-white w3-card w3-padding-small w3-center w3-container viewShare">
 	<span class="w3-medium w3-button w3-display-topright" onClick="document.getElementById('share').style.display = 'none';" style="font-size: 120%;">&times;</span>
-	<div class="w3-container w3-text-theme w3-padding w3-display-topleft vcBold" style="font-size: 120%; margin-bottom: 15px;">
+	<div class="w3-container w3-text-theme w3-padding w3-display-topleft vcBold" style="font-size: 120%;">
 		Share it!
 	</div>
 		
+	<div id="shareRow" style="margin-top: 15px;">	
 		<div id="fbBtn" class="w3-button" value="" onclick="doFB(this.value)">
 			<img src="../branding_icons/flogo-grey.png" alt="fb logo" style="width: 20px" />
 		</div>
@@ -58,6 +59,7 @@ function doTumb(tVal){
 				<input type="url" id="imgUrl" class="w3-input w3-border" value="" />
 					<div class="w3-button w3-gray" onClick="copyText('imgUrl')" style="cursor: pointer;">-></div>
 			</div>
+	</div>
 		</div>
 
 <!-- begin slideshow modal -->
