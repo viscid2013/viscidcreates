44e16324d6952b6429c87559771e5b7f4d809dce
Ben McFadden <viscid07@gmail.com>
2019-08-04 06:35:50 -0400

    moved bio test content to bio body page and updated margin top to 300px

--- v_portfolio/pages/vc_bio.php
+++ v_portfolio/pages/vc_bio.php
@@ -18,9 +18,25 @@ include("../header/vc_header_menus.php");
 </head>
 
 <body>
-<div style="margin-top:300px;">
-	This is the content of the Bio page!
-</div>
+	<div id="bioBody" class="w3-display-container w3-padding-large">
+	
+		<div id="bioHeader" class="w3-card w3-padding w3-display-topleft w3-white w3-opacity" style="margin-bottom: 35px;">
+			<h3 class="w3-text-grey w3-padding-small">Ben McFadden</h3>
+				<h1 class="w3-text-theme w3-padding-small" style="margin-left: 35px;">Viscid Creates</h1>
+		</div>
+		
+			<div id="idDo" class="w3-card w3-padding w3-white w3-opacity">
+				<h3>What I Do</h3>
+				<p>I create illustrations and graphics for a variety of themes and needs, as well as create comics of various sorts.</p>
+				<p>I also am a still-learning web coder and web designer, with a soon-to-be growing body of work on BeAmCreates.com.</p>
+			</div>
+		
+			<div id="idAm" class="w3-card w3-padding w3-white w3-opacity">
+				<h3>Who I Am</h3>
+				<p>I am a dad, an artist, a writer, a librarian, a web coder, a casual gamer, and just the right kind of neurotic that I can masquerade as being disciplined and occasionally appear organized.</p>
+			</div>
+	
+	</div>
 
 <?php
 include("../components/main_script.php");
